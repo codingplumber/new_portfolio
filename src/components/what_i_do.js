5ed4d2c94f2ff8143eea791f17212a53b04338eb
codingplumber <jason.stickel@live.com>
2017-10-16 19:29:28 -0400

    changed link to resume

--- src/components/what_i_do.js
+++ src/components/what_i_do.js
@@ -15,7 +15,7 @@ const WhatIDo = (props) => {
           <Desc hide={!isInView}>
             <H3>What I do</H3>
             <p style={{marginBottom: 22 + 'px'}}>I build websites in JavaScript, Angular, Node, HTML, and CSS and love every minute of it. In fact, I am learning React right now, and I built this portfolio in it!</p>
-            <p>See my resume <a href="https://docs.google.com/document/d/1Z7g110_CEXv1EFnLAgH1xA6-DzFe2A5GAPvebiCKbcM/edit#"><Ul>here</Ul></a>.</p>
+            <p>See my resume <a href="https://docs.google.com/document/d/1Z7g110_CEXv1EFnLAgH1xA6-DzFe2A5GAPvebiCKbcM/edit?usp=sharing"><Ul>here</Ul></a>.</p>
           </Desc>
         }
       </WhenInView>
